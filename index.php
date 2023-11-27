6dd9da55a33433dfccef8fe60144b13b5db5d9a1
Darshit Limbad <darshitlimbad555@gmail.com>
2023-11-27 22:05:10 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include "interface.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Personal Chat</title>
+    <link rel="stylesheet" href="css/index.css" type="text/css"> 
+</head>
+<body>
+    
+    
+</body>
+</html>
---
+++ interface.php
@@ -0,0 +1,66 @@
+<!-- bots app -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="icon" href="img/botsapp_white.png" type="image/png">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/interface.css" type="text/css">
+</head>
+<body>
+    <header>
+        <div class="title">
+            <img src="img/botsapp_white.png">
+            <h1>BotsApp</h1>
+            <p>-A Better place for chat.</p>
+        </div>
+    </header>
+
+    <div class="side-bar">
+        <div class="top">
+            <div class="options">
+                <div class="img">
+                    <img src="img/icons/chat-30.png" alt="Chat">
+                </div>
+                <h6>Personal</h6>
+            </div>
+            
+            <div class="options">
+                <div class="img">
+                    <img src="img/icons/group_chat-48.png" alt="Group">
+                </div>
+                <h6>Groups</h6>
+            </div>
+        </div>
+        
+        <div class="bottom">
+            <div class="options">
+                <div class="img">
+                    <img src="img/icons/share-24.png" alt="Settings">
+                </div>
+            </div>
+    
+            <div class="options">
+                <div class="img">
+                    <img src="img/icons/setting-24.png" alt="Settings">
+                </div>
+            </div>
+            
+            <div class="options profile">
+                <div class="img">
+                    <img src="img/icons/default_dp.png" alt="Settings">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="chat-container">
+
+    </div>
+    
+</body>
+</html>
