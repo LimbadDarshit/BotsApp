87b553999db2da0e7e2c4b00858cb0f9ab4aebd8
Darshit Limbad <darshitlimbad555@gmail.com>
2023-12-13 12:30:06 +0530

    show settings body 50% done

--- js/interface.js
+++ js/interface.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded' , function () {
     // title of the list page
     document.querySelector('#cname').innerHTML = document.title;
 
-                                                        // i can use img or any ele.loaded to stop the animation rather then the whole dom content
+                                                        // i should use img or ele.loaded to stop the skeleton animation rather then the whole dom content
     // skeleton animation stop 
     const ani_ele = document.querySelectorAll('.list img , .list h5 , .list p');
             // animation elements
@@ -20,16 +20,39 @@ document.addEventListener('DOMContentLoaded' , function () {
         element.classList.remove('skeleton-text');
     });
 
-    //li toggle
+    //settings-box setting option toggle
     const li = document.querySelectorAll(".settings-box ul li");
     li.forEach( function (ele) {
         ele.addEventListener('click' , function (){
             if(!(ele.classList.contains('selected')))    {
                 remove_selected_li();
-                ele.classList.toggle('selected');
+                ele.classList.add('selected');
+
+                const bodyname = ele.getAttribute("name") + "-body";
+                console.log(bodyname);
+                const corrosponding_body = document.querySelector(".settings-box .settings-container .body[name="+bodyname+"");
+
+                console.log(corrosponding_body);
+
             }
         })
-    })
+    });
+    console.log();
+
+
+    //profile edit toggle
+    document.querySelectorAll('.edit-icon').forEach( function (edit_icon) {
+        edit_icon.addEventListener('click' , function (){
+            var flex =  edit_icon.closest(".flex");
+            if(flex)    {
+                var text_box = flex.querySelector(".text");
+                text_box.classList.toggle("edit");
+                text_box.toggleAttribute('disabled');
+            }
+        })
+    });
+
+
 });
 
 // toggle settings-box
@@ -38,7 +61,10 @@ function toggle_settings_box()   {
     if (!settings_box.classList.contains('settings-box_show')) {
         settings_box.classList.add('settings-box_show');
         settings_box.classList.remove('settings-box_hide');
-        document.querySelector(".settings-box ul li").classList.add('selected');
+        (!event.target.closest("div[title='Profile']"))  ?
+            document.querySelector(".settings-box ul li[name='general']").classList.add('selected') :
+            document.querySelector(".settings-box ul li[name='profile']").classList.add('selected')    ;
+        
         document.addEventListener('click' , closesettingsbox);
     } else {
         settings_box.classList.add('settings-box_hide');
@@ -61,7 +87,10 @@ function closesettingsbox(event) {
     settings_box =  document.querySelector(".settings-box");
     
 
-    if((!settings_box.contains(event.target)) && (settings_box != event.target) && (!event.target.closest("div[title='Settings']")) ) {
+    if((!settings_box.contains(event.target)) &&
+        (settings_box != event.target) && 
+        (!event.target.closest("div[title='Settings']"))&&
+        (!event.target.closest("div[title='Profile']")) ) {
         toggle_settings_box();
     }
 }
